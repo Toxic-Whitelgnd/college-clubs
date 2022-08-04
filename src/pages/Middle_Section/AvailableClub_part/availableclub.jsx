5caf821de90ae8f975d14365177aa2e8da7a9281
Toxic-Whitelgnd <whitelegend56@gmail.com>
2022-08-04 13:09:06 +0530

    filters added

--- src/pages/Middle_Section/AvailableClub_part/availableclub.jsx
+++ src/pages/Middle_Section/AvailableClub_part/availableclub.jsx
@@ -9,12 +9,15 @@ import Tilt from 'react-parallax-tilt';
 
 import Aos from "aos"
 import "aos/dist/aos.css"
-import { useEffect } from 'react';
+import { useEffect,useState } from 'react';
 
 import * as Siico from "react-icons/si"
 
 
 const Availableclub = () => {
+    // for filters
+    const [Searchterm,setSearchTerm] = useState('');
+    JSON.stringify(data)
     const popover = (
         <Popover id="popover-basic">
           <Popover.Header as="h3">No Accounts</Popover.Header>
@@ -29,10 +32,30 @@ const Availableclub = () => {
     return (
         <div className="AvailableClub">
             <div data-text="Available Club" class="light">Available Clubs</div>
+            {/* for search btn */}
+            <div class="search-box">
+                <button class="btn-search"><i class="fas fa-search"></i></button>
+                <input type="text" class="input-search" placeholder="Type to Search..." 
+                onChange={e => {
+                    setSearchTerm(e.target.value);
+                }}/>
+            </div>
         <div className="card-container">
            
-            {
-                data.map((value,index)=>{
+            {   
+                
+                data.filter((result)=>{
+                    if( Searchterm == ""){
+                        return result
+                    }
+                    else if( result.type.toLocaleLowerCase().includes(Searchterm.toLocaleLowerCase()) ){
+                        return result
+                    }
+                    else if( result.title.toLocaleLowerCase().includes(Searchterm.toLocaleLowerCase())){
+                        return result
+                    }
+                  
+                }).map((value,index)=>{
                     function openweb(link){
                         console.log(link)
                         console.log("clicked")
@@ -40,7 +63,7 @@ const Availableclub = () => {
                     }
 
                     return(
-                        <div className="card-container">
+                        <div className="card-container" key={index}>
                         <Tilt tiltMaxAngleX={15} tiltMaxAngleY={25} >
                         <Card className="card-cc" border={value.border}  key={index} style= {{width: '18rem'}}
                         data-aos={value.framework} data-aos-duration="1000"  data-aos-mirror="true" >
